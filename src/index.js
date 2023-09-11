9d9b54fcf65dd2603322ed449e5b5d614583d251
Phạm Quân <phamquan29hy@gmail.com>
2023-09-11 23:28:24 +0700

    Het kien thuc co ban

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
+
 //template engine
 // app.engine('handlebars', handlebars());
 // app.engine('handlebars', engine());
@@ -21,15 +27,28 @@ app.set('views', './views');
 app.set('views', path.join(__dirname,'resources/views'));
 
 
+//Routes init: khoi tạo tuyến đường
+// route();
+
 // đường dẫn
 app.get('/', (req, res) => {
   // res.send('Hello World!')
   res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  res.render('Search');
+});
+
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
+});
+
 //127.0.0.1 - localhost
 app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
